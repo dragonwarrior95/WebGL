b6262edf81163e6fb72247afa5925b94cb2a84b8
dragonwarrior95 <dragonwarrior95@163.com>
2017-05-17 22:52:20 +0800

    draw point

--- WebGL.js
+++ WebGL.js
@@ -6,6 +6,10 @@ function print(msg) {
     console.log(msg);
 }
 
+function $(id) {
+    return typeof id == "string" ? document.getElementById(id) : id;
+}
+
 var VSHADER_SOURCE =
     'attribute vec4 a_Position;\n' +
     'attribute float a_PointSize;\n' +
@@ -22,7 +26,7 @@ var FSHADER_SOURCE =
     '}'
 
 function main() {
-    var canvas = document.getElementById("canvas");
+    var canvas = $("canvas");
     if (canvas == null) {
         print("canvas is null");
         return;
@@ -38,22 +42,56 @@ function main() {
         return;
     }
 
+    var a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize');
+    if (a_PointSize < 0) {
+        print("get a_PointSize failure......");
+    }
     // 获取a_Position
     var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
     if (a_Position < 0) {
         print("get a_Position failure......");
         return;
     }
-    var a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize');
-    if (a_PointSize < 0) {
-        print("get a_PointSize failure......");
-    }
+    
+    canvas.onmousedown = function (ev) { onMouseDown(ev, gl, canvas, a_Position);};
+    canvas.onmousemove = function (ev) { onMouseMove(ev, gl, canvas, a_Position);};
 
+    gl.vertexAttrib1f(a_PointSize, 10.0);
     gl.vertexAttrib3f(a_Position, 0.5, 0.5, 0.0);
-    gl.vertexAttrib1f(a_PointSize, 5.0);
 
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
 
+    gl.drawArrays(gl.POINTS, 0, 1);
+}
+
+var g_points = [];
+function onMouseDown(ev, gl, canvas, a_Position) {
+    var x = ev.clientX;
+    var y = ev.clientY;
+    var rect = ev.target.getBoundingClientRect();
+
+    x = ((x-rect.left) - canvas.height / 2) / (canvas.height / 2);
+    y = (canvas.width / 2 - (y - rect.top)) / (canvas.width / 2);
+
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.vertexAttrib3f(a_Position, x, y, 0.0);
+    gl.drawArrays(gl.POINTS, 0, 1);
+}
+function onMouseMove(ev, gl, canvas, a_Position) {
+    var x = ev.clientX;
+    var y = ev.clientY;
+    var rect = ev.target.getBoundingClientRect();
+
+    x = ((x-rect.left) - canvas.height / 2) / (canvas.height / 2);
+    y = (canvas.width / 2 - (y - rect.top)) / (canvas.width / 2);
+
+    var div = $("console");
+    div.innerHTML = "point(" + ev.clientX + "," + ev.clientY + ")<br/>gl(" +
+        x.toFixed(2) + "," + y.toFixed(2) + ")";
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.vertexAttrib3f(a_Position, x, y, 0.0);
     gl.drawArrays(gl.POINTS, 0, 1);
 }
