979eee44218fac3dd16167982a57806b5c858f7c
zwl <zwl@meitu.com>
2017-05-18 19:03:44 +0800

    添加相关处理

--- WebGL.js
+++ WebGL.js
@@ -10,6 +10,43 @@ function $(id) {
     return typeof id == "string" ? document.getElementById(id) : id;
 }
 
+function onChange(value) {
+    console.log(value);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.vertexAttrib1f(a_PointSize, value);
+    gl.drawArrays(gl.POINTS, 0, 1);
+}
+
+var red = 0.0;
+var green = 0.0;
+var blue = 0.0;
+var alpha = 0.0;
+function onRValue(value) {
+    red = value;
+    onColorChange();
+}
+
+function onGValue(value) {
+    green = value;
+    onColorChange();
+}
+
+function onBValue(value) {
+    blue = value;
+    onColorChange();
+}
+
+function onAValue(value) {
+    alpha = value;
+    onColorChange();
+}
+
+function onColorChange() {
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.uniform4f(u_FragColor, red / 255.0, green/255.0, blue/255.0, alpha / 255.0);
+    gl.drawArrays(gl.POINTS, 0, 1);
+}
+
 var VSHADER_SOURCE =
     'attribute vec4 a_Position;\n' +
     'attribute float a_PointSize;\n' +
@@ -27,6 +64,10 @@ var FSHADER_SOURCE =
     '    gl_FragColor = u_FragColor;\n' +
     '}'
 
+var a_PointSize;
+var u_FragColor;
+var gl;
+var bLButtonDown = false;
 function main() {
     var canvas = $("canvas");
     if (canvas == null) {
@@ -34,7 +75,7 @@ function main() {
         return;
     }
 
-    var gl = getWebGLContext(canvas, true);
+    gl = getWebGLContext(canvas, true);
     if (gl == null) {
         print("Get gl context failure......");
     }
@@ -44,7 +85,7 @@ function main() {
         return;
     }
 
-    var a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize');
+    a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize');
     if (a_PointSize < 0) {
         print("get a_PointSize failure......");
     }
@@ -55,19 +96,28 @@ function main() {
         return;
     }
 
-    var u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
+    u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
     if (u_FragColor < 0) {
         print("get u_FragColor failure......");
         return;
     }
     
     canvas.onmousedown = function (ev) { onMouseDown(ev, gl, canvas, a_Position);};
+    canvas.onmouseup = function (ev) { onMouseUp(ev, gl, canvas, a_Position);};
     canvas.onmousemove = function (ev) { onMouseMove(ev, gl, canvas, a_Position);};
+
+    canvas.addEventListener("touchStart", function (ev) { onTouchStart(ev, gl, canvas, a_Position);});
+    canvas.addEventListener("touchEnd", function (ev) { onTouchEnd(ev, gl, canvas, a_Position);});
     canvas.addEventListener("touchMove", function (ev) { onTouchMove(ev, gl, canvas, a_Position);});
 
     gl.vertexAttrib1f(a_PointSize, 10.0);
     gl.vertexAttrib3f(a_Position, 0.5, 0.5, 0.0);
-    gl.uniform4f(u_FragColor, 0.0, 0.0, 1.0, 1.0);
+
+    red = 255.0;
+    green = 0.0;
+    blue = 0.0;
+    alpha = 255;
+    gl.uniform4f(u_FragColor, red / 255.0, green/255.0, blue/255.0, alpha / 255.0);
 
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
@@ -77,6 +127,8 @@ function main() {
 
 var g_points = [];
 function onMouseDown(ev, gl, canvas, a_Position) {
+    bLButtonDown = true;
+
     var x = ev.x - canvas.offsetLeft;
     var y = ev.y - canvas.offsetTop;
     var rect = ev.target.getBoundingClientRect();
@@ -92,7 +144,18 @@ function onMouseDown(ev, gl, canvas, a_Position) {
     gl.vertexAttrib3f(a_Position, x, y, 0.0);
     gl.drawArrays(gl.POINTS, 0, 1);
 }
+function onMouseUp(ev, gl, canvas, a_Position) {
+    bLButtonDown = false;
+
+    var x = ev.x - canvas.offsetLeft;
+    var y = ev.y - canvas.offsetTop;
+    var rect = ev.target.getBoundingClientRect();
+
+    var width = canvas.width / 2;
+    var height= canvas.height/2;
+}
 function onMouseMove(ev, gl, canvas, a_Position) {
+    if (bLButtonDown === true) {
     var x = ev.x - canvas.offsetLeft;
     var y = ev.y - canvas.offsetTop;
     var rect = ev.target.getBoundingClientRect();
@@ -110,23 +173,35 @@ function onMouseMove(ev, gl, canvas, a_Position) {
 
     gl.vertexAttrib3f(a_Position, x, y, 0.0);
     gl.drawArrays(gl.POINTS, 0, 1);
+    }
 }
+
+function onTouchStart(ev, gl, canvas, a_Position) {
+    bLButtonDown = true;
+}
+
 function onTouchMove(ev, gl, canvas, a_Position) {
-    var x = ev.x - canvas.offsetLeft;
-    var y = ev.y - canvas.offsetTop;
-    var rect = ev.target.getBoundingClientRect();
+    bLButtonDown = false;
+}
 
-    var width = canvas.width / 2;
-    var height= canvas.height/2;
+function onTouchMove(ev, gl, canvas, a_Position) {
+    if (bLButtonDown === true) {
+        var x = ev.x - canvas.offsetLeft;
+        var y = ev.y - canvas.offsetTop;
+        var rect = ev.target.getBoundingClientRect();
 
-    x = (x - width) / width;
-    y = (height - y) / height;
+        var width = canvas.width / 2;
+        var height = canvas.height / 2;
 
-    var div = $("console");
-    div.innerHTML = "point(" + ev.touches[0].pageX + "," + ev.touches[0].pageY + ")<br/>gl(" +
-        x.toFixed(2) + "," + y.toFixed(2) + ")";
-    gl.clear(gl.COLOR_BUFFER_BIT);
+        x = (x - width) / width;
+        y = (height - y) / height;
 
-    gl.vertexAttrib3f(a_Position, x, y, 0.0);
-    gl.drawArrays(gl.POINTS, 0, 1);
+        var div = $("console");
+        div.innerHTML = "point(" + ev.touches[0].pageX + "," + ev.touches[0].pageY + ")<br/>gl(" +
+            x.toFixed(2) + "," + y.toFixed(2) + ")";
+        gl.clear(gl.COLOR_BUFFER_BIT);
+
+        gl.vertexAttrib3f(a_Position, x, y, 0.0);
+        gl.drawArrays(gl.POINTS, 0, 1);
+    }
 }
