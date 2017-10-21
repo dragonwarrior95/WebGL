312aac36e87d031dcf422f042fdcc187431e8c46
zwl <zwl@meitu.com>
2017-10-21 14:32:08 +0800

    gl封装

--- WebGLImage.js
+++ WebGLImage.js
@@ -93,18 +93,11 @@ function jsReadFiles(files) {
     }
 }
 
-var u_ModelMatrix;// 变换矩阵
-var modelMatrix;  // 变换矩阵值
-var a_TexCoord; // 纹理坐标
-var a_Position; // 顶点坐标
 var vertexBuffer;   // 顶点坐标值
 var texCoordBuffer; // 纹理坐标值
-
-var texture;    // 纹理值
-var u_Sampler;  // 纹理
-
-var gl;
 var bLButtonDown = false;
+let filterBase = null;
+let glTexture;      // gl纹理
 
 function main() {
     var vs = document.getElementById("vs");
@@ -122,10 +115,8 @@ function main() {
         print("Get gl context failure......");
     }
 
-    if (!initShaders(gl, vs.text, fs.text)) {
-        print("init shaders error......");
-        return;
-    }
+    filterBase = new JFilterBase(gl);
+    filterBase.initlize();
 
     // canvas.onmousedown = function (ev) { onMouseDown(ev, gl, canvas, a_Position);};
     // canvas.onmouseup = function (ev) { onMouseUp(ev, gl, canvas, a_Position);};
@@ -144,37 +135,8 @@ function main() {
     // canvas.addEventListener("touchstart", function (ev) { onTouchStart(ev, gl, canvas, a_Position);});
     // canvas.addEventListener("touchend", function (ev) { onTouchEnd(ev, gl, canvas, a_Position);});
     // canvas.addEventListener("touchmove", function (ev) { onTouchMove(ev, gl, canvas, a_Position);});
-
-    // 获取a_Position
-    a_Position = gl.getAttribLocation(gl.program, 'a_Position');
-    if (a_Position < 0) {
-        print("get a_Position failure......");
-        return;
-    }
-    a_TexCoord = gl.getAttribLocation(gl.program, 'a_TexCoord');
-
-    var n = initVertexBuffers(gl);// 初始化顶点
-    if (n < 0) {
-        return;
-    }
-    initTexture(gl);// 初始化纹理
-
-    // 设置变换矩阵
-    u_ModelMatrix = gl.getUniformLocation(gl.program, 'u_ModelMatrix');
-    if (u_ModelMatrix < 0) {
-        print("get u_ModelMatrix failure");
-        return;
-    }
-
-    modelMatrix = new Matrix4();
-    modelMatrix.setScale(1.0, 1.0, 1.0);
-    gl.uniformMatrix4fv(u_ModelMatrix, false, modelMatrix.elements);
-
-
-    gl.clearColor(0.6, 0.6, 0.6, 1.0);
-    gl.clear(gl.COLOR_BUFFER_BIT);
-
     onLoadImage("1.jpg");
+
 
     // gl.drawArrays(gl.TRIANGLE_FAN, 0, n);
 }
@@ -190,186 +152,64 @@ function animal(angle) {
     return newAngle %= 360;
 }
 
-function draw(gl, n, angle, modelMatrix, u_ModelMatrix) {
-    modelMatrix.setRotate(angle, 0, 0, 1);
-    gl.uniformMatrix4fv(u_ModelMatrix, false, modelMatrix.elements);
-    gl.clear(gl.COLOR_BUFFER_BIT);
-    gl.drawArrays(gl.LINE_LOOP, 0, n);
-}
+function draw(canvas) {
+    let width = canvas.width;
+    let height= canvas.height;
 
-function initVertexBuffers(gl) {
-    var n = 4;
-
-    // 创建缓冲区对象
-    vertexBuffer = gl.createBuffer();
-    if (!vertexBuffer) {
-        print("create Buffer failure......");
-        return -1;
+    if (!filterBase || !glTexture) {
+        return;
     }
+    let webGL = filterBase.getWebGL();
+    webGL.clearColor(160.0/255.0, 160.0/255.0, 160.0/255.0, 1.0);
+    webGL.clear(gl.COLOR_BUFFER_BIT);
+    let Projection = new Matrix4();
+    Projection.setIdentity();
+    Projection.ortho(0.0, width, height, -1.0, 1.0);
+    let TRSMat = new Matrix4();
+    TRSMat.scale(1.0, 1.0, 1.0);
+    TRSMat.translate(width/2.0, height/2.0, 0.0);
 
-    texCoordBuffer = gl.createBuffer();
-    if (!texCoordBuffer) {
-        print("create Buffer failure......");
-        return -1;
-    }
-    return n;
-}
+    // QRect rcShow = CenterScaleImage(m_nTextureWidth, m_nTextureHeight, nWidth, nHeight);
+    // GLfloat vertexs[8] = {
+    //     -rcShow.width() / 2.0f, rcShow.height() / 2.0f,
+    //     rcShow.width() / 2.0f, rcShow.height() / 2.0f,
+    //     -rcShow.width() / 2.0f, -rcShow.height() / 2.0f,
+    //     rcShow.width() / 2.0f, -rcShow.height() / 2.0f
+    // };
+// 	GLfloat vertexs[8] = { 0.0f, 0.0f,
+// 		(float)m_nTextureWidth, 0,
+// 		0.0f, (float)m_nTextureHeight,
+// 		(float)m_nTextureWidth, (float)m_nTextureHeight};
 
-function initTexture(gl) {
-    texture = gl.createTexture();
-    u_Sampler = gl.getUniformLocation(gl.program, 'u_Sampler');
+    let vertexs = new  Float32Array([
+        0.0, 0.0,
+        width, 0.0,
+        0.0, height,
+        width, height
+    ]);
+    let texcoords = new Float32Array([
+        0.0,0.0,
+        1.0,0.0,
+        0.0,1.0,
+        1.0,1.0
+    ]);
+    filterBase.useProgram();
+    filterBase.initlize();
+    filterBase.bind(glTexture, glTexture.width, glTexture.height);
+    filterBase.filterToScreenSample(Projection.multiply(TRSMat), vertexs, texcoords, width, height);
 }
 
 function onLoadImage(fileName) {
     var image = new Image();
     image.onload = function () {
-        setAutoShow(gl, image.width, image.height);// 图片加载为设置自适应
+        // setAutoShow(gl, image.width, image.height);// 图片加载为设置自适应
 
-        var filterBase = new JFilterBase(gl, image.width, image.height);
-        filterBase.bind(texture, image.width, image.height);
-        filterBase.bindFBO();
-        loadTexture(filterBase.getWebGL(), texture, u_Sampler, image);// 加载纹理到纹理中
-        let result_texture = filterBase.filterToFBO(true);
-        filterBase.unBindFBO();
-        loadTextureFromTexture(filterBase.getWebGL(), result_texture, u_Sampler);// 加载纹理
+        glTexture = filterBase.loadTexture(image);
+
+        var canvas = $("canvas");
+        draw(canvas);
     };
     image.src = fileName;
-}
-
-function loadTextureFromTexture(gl, texture, u_sampler) {
-    gl.activeTexture(gl.TEXTURE0);
-    gl.bindTexture(gl.TEXTURE_2D, texture);
-    gl.uniform1i(u_sampler, 0);
-
-    gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
-}
-
-function loadTexture(gl, texture, u_Sampler, image) {
-    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1);// 对纹理图片进行Y轴反转
-    gl.activeTexture(gl.TEXTURE0);
-    gl.bindTexture(gl.TEXTURE_2D, texture);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
-    gl.uniform1i(u_Sampler, 0);
-
-    gl.drawArrays(gl.TRIANGLE_FAN, 0, 4);
-}
-
-function setAutoShow(gl, imgWidth, imgHeight) {
-    var canvas = $("canvas");
-    var canvasWidth = canvas.width; // 画布宽高
-    var canvasHeight= canvas.height;
-
-    var startXonCanvas; // 图片在画布上显示的起始位置
-    var startYonCanvas;
-    var startXonImg;    // 图片显示的起始位置
-    var startYonImg;
-    var imgShowWidth;   // 图片显示宽高
-    var imgShowHeight;
-    var scale = 1.0;    // 缩放倍数
-
-    if (canvasWidth >= imgWidth && canvasHeight >= imgHeight) {
-        // 画布大
-        startXonCanvas = (canvasWidth - imgWidth) / 2.0;
-        startYonCanvas = (canvasHeight - imgHeight) / 2.0;
-        imgShowWidth = imgWidth;
-        imgShowHeight = imgHeight;
-    }
-    else {
-        var percentW = canvasWidth / imgWidth;
-        var percentH = canvasHeight / imgHeight;
-
-        if (percentH < percentW)
-            scale = percentH;
-        else
-            scale = percentW;
-
-        //得到缩略图的显示大小
-        imgShowWidth = imgWidth * scale;
-        imgShowHeight = imgHeight * scale;
-
-        startXonCanvas = (canvasWidth - imgShowWidth) / 2.0;
-        startYonCanvas = (canvasHeight - imgShowHeight) / 2.0;
-    }
-    startXonImg = 0.0;
-    startYonImg = 0.0;
-
-    // 转换为WebGL的顶点坐标
-    var width = canvas.width / 2;
-    var height= canvas.height/2;
-
-    // canvas上的坐标转换为WebGL上的坐标
-    // x1 = (y - width) / width;
-    // y1 = (height - x) / height;
-    // WebGL上的坐标对应图片上的坐标
-    // var vertices = new Float32Array([
-    //     -1.0, 1.0, 0.0, 1.0,
-    //     -1.0, -1.0, 0.0, 0.0,
-    //     1.0, -1.0, 1.0, 0.0,
-    //     1.0, 1.0, 1.0, 1.0
-    // ]);
-
-    // // 顶点坐标
-    // var vertices = new Float32Array([
-    //     (startXonCanvas - width) / width,               (height - startYonCanvas) / height,
-    //     (startXonCanvas - width) / width,               (height - (startYonCanvas + imgShowHeight)) / height,
-    //     (startXonCanvas + imgShowWidth - width) / width,(height - (startYonCanvas + imgShowHeight)) / height,
-    //     (startXonCanvas + imgShowWidth - width) / width,(height - startYonCanvas) / height
-    // ]);
-    //
-    // //　图片坐标
-    // var texCoords = new Float32Array([
-    //     startXonImg / imgWidth,                 startYonImg / imgHeight,
-    //     startXonImg / imgWidth,                 (startYonImg + imgShowHeight) / imgHeight,
-    //     (startXonImg + imgShowWidth) / imgWidth,startYonImg / imgHeight,
-    //     (startXonImg + imgShowWidth) / imgWidth,(startYonImg + imgShowHeight) / imgHeight
-    // ]);
-
-    // 顶点坐标图片坐标
-    // var verticesTexCoords = new Float32Array([
-    //     (startXonCanvas - width) / width,               (height - startYonCanvas) / height,                     startXonImg / imgWidth,                 (imgHeight - startYonImg) / imgHeight,
-    //     (startXonCanvas - width) / width,               (height - (startYonCanvas + imgShowHeight)) / height,   startXonImg / imgWidth,                 (imgHeight - startYonImg - imgShowHeight) / imgHeight,
-    //     (startXonCanvas + imgShowWidth - width) / width,(height - (startYonCanvas + imgShowHeight)) / height,   (startXonImg + imgShowWidth) / imgWidth,(imgHeight - startYonImg) / imgHeight,
-    //     (startXonCanvas + imgShowWidth - width) / width,(height - startYonCanvas) / height,                     (startXonImg + imgShowWidth) / imgWidth,(imgHeight - startYonImg - imgShowHeight) / imgHeight
-    // ]);
-
-    var verticesTexCoords = new Float32Array([
-        (startXonCanvas - width) / width,               (height - startYonCanvas) / height,                     0.0,1.0,
-        (startXonCanvas - width) / width,               (height - (startYonCanvas + imgShowHeight)) / height,   0.0,0.0,
-        (startXonCanvas + imgShowWidth - width) / width,(height - (startYonCanvas + imgShowHeight)) / height,   1.0,0.0,
-        (startXonCanvas + imgShowWidth - width) / width,(height - startYonCanvas) / height,                     1.0,1.0
-    ]);
-
-    // 创建缓冲区对象
-    if (vertexBuffer) {
-        gl.deleteBuffer(vertexBuffer);
-    }
-    vertexBuffer = gl.createBuffer();
-    if (!vertexBuffer) {
-        print("create Buffer failure......");
-        return -1;
-    }
-    // 将缓冲区对象绑定到目标
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    // 向缓冲区对象写入数据
-    gl.bufferData(gl.ARRAY_BUFFER, verticesTexCoords, gl.STATIC_DRAW);
-    // 将缓冲区对象分配给a_Position变量
-    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, verticesTexCoords.BYTES_PER_ELEMENT * 4, 0);
-    // 连接a_Position变量与分配给他的缓冲区对象
-    gl.enableVertexAttribArray(a_Position);
-
-    if (texCoordBuffer) {
-        gl.deleteBuffer(texCoordBuffer);
-    }
-    texCoordBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, texCoordBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, verticesTexCoords, gl.STATIC_DRAW);
-    gl.vertexAttribPointer(a_TexCoord, 2, gl.FLOAT, false, verticesTexCoords.BYTES_PER_ELEMENT * 4, verticesTexCoords.BYTES_PER_ELEMENT * 2);
-    gl.enableVertexAttribArray(a_TexCoord);
-
-    $("scale").value = scale*100;
 }
 
 // 获取图片完整地址打开图片
--- webgl/JFilterBase.js
+++ webgl/JFilterBase.js
@@ -5,13 +5,14 @@
  */
 
 class JFilterBase {
-    constructor(webGL, width, height) {
+    constructor(webGL) {
+        var self = this;
         this.m_webGL = webGL;           // GL对象
         this.m_FrameBufferTexture = 0;// 离屏渲染纹理
         this.m_FrameBufferObj = 0;     // 帧缓冲区对象
         this.m_RenderBufferObj = 0;    // 渲染缓冲区对象
-        this.m_textureWidth = width;
-        this.m_textureHeight = height;
+        this.m_textureWidth = 0;
+        this.m_textureHeight = 0;
 
 
         this.m_vertexBuffer = null;   // 顶点坐标值
@@ -38,6 +39,13 @@ class JFilterBase {
         this.a_TexCoordHandle = this.m_webGL.getAttribLocation(this.m_webGL.program, "a_TexCoord");
         this.u_ModelMatrixHandle = this.m_webGL.getUniformLocation(this.m_webGL.program, "u_ModelMatrix");
         this.u_SamplerHandle = this.m_webGL.getUniformLocation(this.m_webGL.program, "u_Sampler");
+    }
+
+    useProgram() {
+        if (this.m_webGL.program)
+            this.m_webGL.useProgram(this.m_webGL.program);
+        else
+            print("m_webGL.program is null......");
     }
 
     release() {
@@ -85,15 +93,28 @@ class JFilterBase {
         this.m_textureWidth = width;
         this.m_textureHeight = height;
     }
-    // 绑定纹理
-    bindTexture () {
-        this.m_webGL.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1);// 对纹理图片进行Y轴反转
+    loadTexture (image) {
+        if (this.m_textureId) {
+            this.m_webGL.deleteTexture(this.m_textureId);
+        }
+        this.m_textureId = this.m_webGL.createTexture();
+        this.m_webGL.pixelStorei(this.m_webGL.UNPACK_FLIP_Y_WEBGL, 1);// 对纹理图片进行Y轴反转
 
         this.m_webGL.activeTexture(this.m_webGL.TEXTURE0);
-        this.m_webGL.bindTexture(gl.TEXTURE_2D, this.m_textureId);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-        gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+        this.m_webGL.bindTexture(this.m_webGL.TEXTURE_2D, this.m_textureId);
+        this.m_webGL.texParameteri(this.m_webGL.TEXTURE_2D, this.m_webGL.TEXTURE_MIN_FILTER, this.m_webGL.LINEAR);
+        this.m_webGL.texParameteri(this.m_webGL.TEXTURE_2D, this.m_webGL.TEXTURE_WRAP_S, this.m_webGL.CLAMP_TO_EDGE);
+        this.m_webGL.texParameteri(this.m_webGL.TEXTURE_2D, this.m_webGL.TEXTURE_WRAP_T, this.m_webGL.CLAMP_TO_EDGE);
+
+        this.m_textureWidth = image.width;
+        this.m_textureHeight = image.height;
+
+        return this.m_textureId;
+    }
+    // 绑定纹理
+    bindTexture() {
+        this.m_webGL.activeTexture(this.m_webGL.TEXTURE0);
+        this.m_webGL.bindTexture(this.m_webGL.TEXTURE_2D, this.m_textureId);
         this.m_webGL.uniform1i(this.u_SamplerHandle, 0);
     }
 
@@ -125,7 +146,6 @@ class JFilterBase {
             void main()
             {
                 gl_FragColor = texture2D(u_Sampler, v_TexCoord);
-            // '    gl_FragColor = vec4(1.0, 0, 0, 1.0);\\n' +
             }
         `);
     }
@@ -137,7 +157,7 @@ class JFilterBase {
     // 创建一个空的纹理
     createTexture (width, height)
     {
-        var v = this.m_webGL.createTexture();
+        var texture = this.m_webGL.createTexture();
         if(texture == 0)
         {
             return 0;
@@ -211,12 +231,29 @@ class JFilterBase {
             return 0;
         }
 
+        this.useProgram();
         this.m_webGL.viewport(0, 0, this.m_textureWidth, this.m_textureHeight);// 设置离屏渲染区域
-        this.m_webGL.uniformMatrix4fv(this.u_ModelMatrixHandle, false, this.m_modelMatrix.elements);
-        this.initVertexBuffers(this.m_textureWidth, this.m_textureHeight);
 
-        this.bindTexture();
-        // this.m_webGL.last_flush();
+        this.m_modelMatrix = new Matrix4();
+        this.m_modelMatrix.ortho(0.0, this.m_textureWidth, 0.0, this.m_textureHeight, -1.0, 1.0);
+        // GLfloat vertexs[8] = { 0.0f, (float)m_TextureHeight,
+        // (float)m_TextureWidth, (float)m_TextureHeight,
+        // 0.0f, 0.0f,
+        // (float)m_TextureWidth, 0 };
+        // GLfloat texcoords[8] = { 0.0f, 1.0f,
+        // 1.0f, 1.0f,
+        // 0.0f, 0.0f,
+        // 1.0f, 0.0f };
+        let vertexBufferData = new Float32Array([
+            0.0,this.m_TextureHeight,            0.0,1.0,
+            this.m_TextureWidth, this.m_TextureHeight,1.0,1.0,
+            0.0,0.0,                        0.0,0.0,
+            this.m_TextureWidth,0.0,             1.0,0.0
+        ]);
+        this.setUniformMatrix4fv("u_ModelMatrix", false, this.m_modelMatrix.elements);// 设置u_ModelMatrix变量
+        this.setVertexAttribPointer("a_Position", this.m_vertexBuffer, vertexBufferData, 2, this.m_webGL.FLOAT, false, vertexBufferData.BYTES_PER_ELEMENT*4, vertexBufferData.BYTES_PER_ELEMENT*0);
+        this.setVertexAttribPointer("a_TexCoord", this.m_texCoordBuffer, vertexBufferData, 2, this.m_webGL.FLOAT, false, vertexBufferData.BYTES_PER_ELEMENT*4, vertexBufferData.BYTES_PER_ELEMENT*2);
+        this.bindTexture();// 设置纹理变量
 
         if(bSavePixels)
         {
@@ -226,8 +263,180 @@ class JFilterBase {
         return this.m_FrameBufferTexture;
     }
 
+    // virtual void FilterToScreen(GLfloat *mvp, GLfloat vertexs[8], GLfloat texcoords[8], int ScreenWidth, int ScreenHeight);// 渲染到屏幕
+    // virtual void FilterToScreenSample(GLfloat *mvp, GLfloat vertexs[8], GLfloat texcoords[8], int ScreenWidth, int ScreenHeight);// 普通渲染(不执行任何特效)
+    filterToScreen(uMatrix, vertexs, texcoords, screenWidth, screenHeight)
+    {
+        if (this.filterToFBO() && this.m_webGL.program)
+        {
+            this.m_webGL.viewport(0, 0, screenWidth, screenHeight);
+            this.useProgram();
+            this.m_webGL.activeTexture(this.m_webGL.TEXTURE0);
+            this.m_webGL.bindTexture(this.m_webGL.TEXTURE_2D, this.m_FrameBufferTexture);
+            this.m_webGL.uniform1i(this.u_SamplerHandle, 0);
+
+            this.setUniformMatrix4fv("u_ModelMatrix", false, uMatrix.elements);// 设置u_ModelMatrix变量
+            this.setVertexAttribPointer("a_Position", this.m_vertexBuffer, vertexs, 2, this.m_webGL.FLOAT, false, 0, 0);
+            this.setVertexAttribPointer("a_TexCoord", this.m_texCoordBuffer, texcoords, 2, this.m_webGL.FLOAT, false, 0, 0);
+
+            this.m_webGL.drawArrays(this.m_webGL.TRIANGLE_STRIP, 0, 4);
+        }
+    }
+    filterToScreenSample(uMatrix, vertexs, texcoords, screenWidth, screenHeight)
+    {
+        if (this.m_textureId && this.m_webGL.program)
+        {
+            this.m_webGL.viewport(0, 0, screenWidth, screenHeight);
+            this.useProgram();
+            this.m_webGL.activeTexture(this.m_webGL.TEXTURE0);
+            this.m_webGL.bindTexture(this.m_webGL.TEXTURE_2D, this.m_textureId);
+            this.m_webGL.uniform1i(this.u_SamplerHandle, 0);
+
+            this.setUniformMatrix4fv("u_ModelMatrix", false, uMatrix.elements);// 设置u_ModelMatrix变量
+            this.setVertexAttribPointer("a_Position", this.m_vertexBuffer, vertexs, 2, this.m_webGL.FLOAT, false, 0, 0);
+            this.setVertexAttribPointer("a_TexCoord", this.m_texCoordBuffer, texcoords, 2, this.m_webGL.FLOAT, false, 0, 0);
+
+            this.m_webGL.drawArrays(this.m_webGL.TRIANGLE_STRIP, 0, 4);
+        }
+    }
+
+
     getWebGL ()
     {
         return this.m_webGL;
+    }
+
+    setUniformli(strKey, value)
+    {
+        let uHandle = this.m_webGL.getUniformLocation(this.m_webGL.program, strKey);
+        this.m_webGL.uniform1i(uHandle, value);
+    }
+
+    setUniform2f(strKey, x, y)
+    {
+        let uHandle = this.m_webGL.getUniformLocation(this.m_webGL.program, strKey);
+        this.m_webGL.uniform2f(uHandle, x, y);
+    }
+
+    setUniform3f(strKey, x, y, z)
+    {
+        let uHandle = this.m_webGL.getUniformLocation(this.m_webGL.program, strKey);
+        this.m_webGL.uniform3f(uHandle, x, y, z);
+    }
+
+    setUniform4f(strKey, r, g, b, a)
+    {
+        let uHandle = this.m_webGL.getUniformLocation(this.m_webGL.program, strKey);
+        this.m_webGL.uniform4f(uHandle, r, g, b, a);
+    }
+
+    /**
+     * @param {WebGLUniformLocation} strKey
+     * @param {boolean} transpose
+     * @param {Float32Array|Array.<number>} data
+     */
+    setUniformMatrix4fv(strKey, transpose, data)
+    {
+        let uHandle = this.m_webGL.getUniformLocation(this.m_webGL.program, strKey);
+        this.m_webGL.uniformMatrix4fv(uHandle, transpose, data);
+    }
+
+    setVertexAttrib1f(strKey, value)
+    {
+        let aHandle = this.m_webGL.getAttribLocation(this.m_webGL.program, strKey);
+        if (aHandle) {
+            this.m_webGL.vertexAttrib1f(aHandle, value);
+        }
+    }
+
+    setVertexAttrib2f(strKey, x, y)
+    {
+        let aHandle = this.m_webGL.getAttribLocation(this.m_webGL.program, strKey);
+        if (aHandle) {
+            this.m_webGL.vertexAttrib2f(aHandle, x, y);
+        }
+    }
+
+    setVertexAttrib3f(strKey, x, y, z)
+    {
+        let aHandle = this.m_webGL.getAttribLocation(this.m_webGL.program, strKey);
+        if (aHandle) {
+            this.m_webGL.vertexAttrib3f(aHandle, x, y, z);
+        }
+    }
+
+    setVertexAttrib4f(strKey, r, g, b, a)
+    {
+        let aHandle = this.m_webGL.getAttribLocation(this.m_webGL.program, strKey);
+        if (aHandle) {
+            this.m_webGL.vertexAttrib4f(aHandle, r, g, b, a);
+        }
+    }
+
+    /**
+     * @param {number} indx
+     * @param {Float32Array|Array.<number>} values
+     */
+    setVertexAttriblfv(strKey, values)
+    {
+        let aHandle = this.m_webGL.getAttribLocation(this.m_webGL.program, strKey);
+        if (aHandle) {
+            this.m_webGL.vertexAttrib1fv(aHandle, values);
+        }
+    }
+    setVertexAttrib2fv(strKey, values)
+    {
+        let aHandle = this.m_webGL.getAttribLocation(this.m_webGL.program, strKey);
+        if (aHandle) {
+            this.m_webGL.vertexAttrib2fv(aHandle, values);
+        }
+    }
+    setVertexAttrib3fv(strKey, values)
+    {
+        let aHandle = this.m_webGL.getAttribLocation(this.m_webGL.program, strKey);
+        if (aHandle) {
+            this.m_webGL.vertexAttrib3fv(aHandle, values);
+        }
+    }
+    setVertexAttrib4fv(strKey, values)
+    {
+        let aHandle = this.m_webGL.getAttribLocation(this.m_webGL.program, strKey);
+        if (aHandle) {
+            this.m_webGL.vertexAttrib4fv(aHandle, values);
+        }
+    }
+
+    /**
+     * @param {string} strKey
+     * @param {} vertexBuffer
+     * @param {Float32Array|Array.<number>} vertexBufferData
+     * @param {number} size, 指定几个分量，一维：1，二维：x, y 三维：x, y, z。。。
+     * @param {number} type 数据类型, 如：this.m_webGL.FLOAT
+     * @param {boolean} normalized， 是否将数据归一化到[0,1]或者[-1,1]
+     * @param {number} stride 指定相邻两个顶点间的字节数 vertexBufferData.BYTES_PER_ELEMENT * 4
+     * @param {number} offset 指定缓冲区的偏移量，即Attribute变量从缓冲区中何处开始存储 vertexBufferData.BYTES_PRE_ELEMENT * 0
+     */
+    setVertexAttribPointer(strKey, vertexBuffer, vertexBufferData, size, type, normalized, stride, offset)
+    {
+        let aHandle = this.m_webGL.getAttribLocation(this.m_webGL.program, strKey);
+        if (aHandle) {
+            // 创建缓冲区对象
+            if (vertexBuffer) {
+                this.m_webGL.deleteBuffer(vertexBuffer);
+            }
+            vertexBuffer = this.m_webGL.createBuffer();
+            if (!vertexBuffer) {
+                print("create Buffer failure......");
+                return -1;
+            }
+            // 将缓冲区对象绑定到目标
+            this.m_webGL.bindBuffer(this.m_webGL.ARRAY_BUFFER, vertexBuffer);
+            // 向缓冲区对象写入数据
+            this.m_webGL.bufferData(this.m_webGL.ARRAY_BUFFER, vertexBufferData, this.m_webGL.STATIC_DRAW);
+            // 将缓冲区对象分配给a_Position变量
+            this.m_webGL.vertexAttribPointer(aHandle, size, type, normalized, stride, offset);
+            // 连接a_Position变量与分配给他的缓冲区对象
+            this.m_webGL.enableVertexAttribArray(aHandle);
+        }
     }
 }
